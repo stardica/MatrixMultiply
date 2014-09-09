acd017f392cf3b74d737f343056911a93e7ba5ed
stardica <stardica@gmail.com>
2014-09-09 10:41:22 -0400

    added mode 2 with some test code.

--- src/MatrixMultiply.c
+++ src/MatrixMultiply.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include "rdtsc.h"
 
+#define SIZE 4
+#define MODE 2
 
-#define SIZE 10
-#define MODE 1
+//function declarations
+void print_me(char *string);
+
+//objects
+struct Object{
+
+	char *string;
+	void (*print_me)(void *ptr);
+	struct Object *next;
+
+};
+
+struct Object object;
+struct Object *object_ptr;
 
 int matA[SIZE][SIZE];
 int matB[SIZE][SIZE];
@@ -79,9 +94,52 @@ int main(int argc, char *argv[]){
 		printf("\nDone. Number of clock Cycles: %llu\n", b-a);
 	}
 
-	else {
-		printf("select another mode\n");
+	else if (MODE == 2)
+	{
+
+		printf("size of long long is %d\n", sizeof(long long));
+		printf("size of long is %d\n", sizeof(long));
+		printf("size of int is %d\n", sizeof(int));
+		printf("size of short is %d\n", sizeof(short));
+
+		char *string = "test sting";
+		printf("Here is the string 1: \"%s\"\n", string);
+
+		//Using the struct
+		//set string variable and point to print_me.
+		object.string = strdup(string);
+		object.print_me = (void (*)(void *)) print_me;
+
+		//use of print_me
+		object.print_me(object.string);
+
+		//pointer fun
+		struct Object *ptr = &object;
+		printf("this is the value of the pointer to struct object: %p\n", ptr);
+		object.next=&object;
+		printf("this is the value of the pointer to struct object: %p\n", object.next);
+		object_ptr = &object;
+		object_ptr->next = &object;
+		printf("this is the value of the pointer to struct object: %p\n", object_ptr->next);
+		printf("done\n");
+
+
+	}
+	else
+	{
+
+		printf("Invalid mode");
+
 	}
 
 	return 1;
+}
+
+
+
+void print_me(char *string)
+{
+
+	printf("Here is the string 2: \"%s\"\n", string);
+
 }
