b0af92dde8c25f72117f1bf27c1ad486e8afb669
stardica <stardica@gmail.com>
2014-09-17 17:48:47 -0400

    added some OpenCL code to Mode 3.

--- src/MatrixMultiply.c
+++ src/MatrixMultiply.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <string.h>
 #include "rdtsc.h"
+#include <CL/cl.h>
 
 
 //SIZE sets height and width of matrix
@@ -11,7 +12,7 @@
 //MODE 1 = single thread Matrix Multiply
 //MODE 2 = Multi thread Matrix Multiply
 #define SIZE 2
-#define MODE 2
+#define MODE 3
 
 //macros
  #define PRINT(...) printf("Print from the Macro: %p %p\n", __VA_ARGS__)
@@ -49,7 +50,74 @@ void PrintMatrices(void);
 
 int main(int argc, char *argv[]){
 
-	if (MODE == 2){
+	if (MODE == 3){
+	    int i, j;
+	    char* value;
+	    size_t valueSize;
+	    cl_uint platformCount;
+	    cl_platform_id* platforms;
+	    cl_uint deviceCount;
+	    cl_device_id* devices;
+	    cl_uint maxComputeUnits;
+
+	    // get all platforms
+	    clGetPlatformIDs(0, NULL, &platformCount);
+	    platforms = (cl_platform_id*) malloc(sizeof(cl_platform_id) * platformCount);
+	    clGetPlatformIDs(platformCount, platforms, NULL);
+
+	    for (i = 0; i < platformCount; i++) {
+
+	        // get all devices
+	        clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, NULL, &deviceCount);
+	        devices = (cl_device_id*) malloc(sizeof(cl_device_id) * deviceCount);
+	        clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, deviceCount, devices, NULL);
+
+	        // for each device print critical attributes
+	        for (j = 0; j < deviceCount; j++) {
+
+	            // print device name
+	            clGetDeviceInfo(devices[j], CL_DEVICE_NAME, 0, NULL, &valueSize);
+	            value = (char*) malloc(valueSize);
+	            clGetDeviceInfo(devices[j], CL_DEVICE_NAME, valueSize, value, NULL);
+	            printf("%d. Device: %s\n", j+1, value);
+	            free(value);
+
+	            // print hardware device version
+	            clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, 0, NULL, &valueSize);
+	            value = (char*) malloc(valueSize);
+	            clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, valueSize, value, NULL);
+	            printf(" %d.%d Hardware version: %s\n", j+1, 1, value);
+	            free(value);
+
+	            // print software driver version
+	            clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, 0, NULL, &valueSize);
+	            value = (char*) malloc(valueSize);
+	            clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, valueSize, value, NULL);
+	            printf(" %d.%d Software version: %s\n", j+1, 2, value);
+	            free(value);
+
+	            // print c version supported by compiler for device
+	            clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, 0, NULL, &valueSize);
+	            value = (char*) malloc(valueSize);
+	            clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, valueSize, value, NULL);
+	            printf(" %d.%d OpenCL C version: %s\n", j+1, 3, value);
+	            free(value);
+
+	            // print parallel compute units
+	            clGetDeviceInfo(devices[j], CL_DEVICE_MAX_COMPUTE_UNITS,
+	            sizeof(maxComputeUnits), &maxComputeUnits, NULL);
+	            printf(" %d.%d Parallel compute units: %d\n", j+1, 4, maxComputeUnits);
+
+	        }
+
+	        free(devices);
+
+	    }
+
+	    free(platforms);
+
+	}
+	else if (MODE == 2){
 
 		printf("---Multi Thread Mode---\n\n");
 		int i, j, k;
